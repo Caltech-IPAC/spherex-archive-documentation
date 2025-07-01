3f2d2c29eb16512e9b8320db806885f505703d00
Brigitta Sipőcz <bsipocz@gmail.com>
2025-07-01 15:11:19 -0700

    MAINT: adding rendering

---
+++ conf.py
@@ -0,0 +1,69 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# -- Project information -----------------------------------------------------
+
+project = 'SPHEREx Archive Documentation'
+copyright = '2025 SPHEREx Archive -- IPAC/IRSA'
+author = 'SPHEREx Archive -- IPAC/IRSA'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'myst_nb',
+    'sphinx_copybutton',
+]
+
+myst_enable_extensions = [
+    'dollarmath',
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'notes', '.tox', '.tmp', '.pytest_cache', ]
+
+# Top level README file's sole purpose is for the repo.
+exclude_patterns += ['README.md',]
+
+# MyST-NB configuration
+nb_execution_timeout = 1200
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_book_theme'
+html_title = 'SPHEREx Archive Documentation'
+html_logo = '_static/irsa_logo.png'
+#html_favicon = '_static/favicon.ico'
+html_theme_options = {
+    "github_url": "https://github.com/Caltech-IPAC/SPHEREx-archive-documentation",
+    "repository_url": "https://github.com/Caltech-IPAC/SPHEREx-archive-documentation",
+    "repository_branch": "main",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_edit_page_button": True,
+    "launch_buttons": {"binderhub_url": "https://mybinder.org",},
+    "logo": {
+        "link": "https://irsa.ipac.caltech.edu/",
+        "alt_text": "NASA/IPAC Infrared Science Archive - Home",
+    },
+    "home_page_in_toc": True,
+}
+
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# myst configurations
+myst_heading_anchors = 4
---
+++ index.md
@@ -0,0 +1,42 @@
+# SPHEREx Archive at IRSA User Guide
+Last updated 13 June 2025
+
+
+
+## Introduction
+
+### Document Purpose and Scope
+The purpose of this document is to facilitate science with SPHEREx data by providing users with an overview of the SPHEREx data that are available at the NASA/IPAC Infrared Science Archive (IRSA) at Caltech, as well as instructions for accessing and downloading these data. We also provide tips for exploring the data and getting help with any questions you may have. This User Guide will evolve as the SPHEREx project delivers new data products and tools to IRSA to make available to the public.
+
+### SPHEREx Overview
+
+SPHEREx is a NASA Astrophysics Medium Explorer mission that launched in March 2025. During its planned two-year mission, SPHEREx will obtain 0.75-5 micron spectroscopy over the entire sky, with deeper data in the SPHEREx Deep Fields. SPHEREx data will be used to:
+
+* **constrain the physics of inflation** by measuring its imprints on the three-dimensional large-scale distribution of matter,
+* **trace the history of galactic light production** through a deep multi-band measurement of large-scale clustering,
+* **investigate the abundance and composition of water and biogenic ices** in the early phases of star and planetary disk formation.
+
+The community will also mine SPHEREx data and combine it with synergistic data sets to address a variety of additional topics in astrophysics.
+
+More information is available in the "Mission Overview" section of the SPHEREx Explanatory Supplement.
+
+
+```{toctree}
+---
+maxdepth: 1
+caption: SHEREx Data at IRSA
+---
+
+
+documentation/spherex_data_products
+documentation/spherex_data_access
+```
+
+
+### Additional Resources
+
+[SPHEREx archive at IRSA](https://irsa.ipac.caltech.edu/Missions/spherex.html)
+
+[SPHEREx project website](https://spherex.caltech.edu/)
+
+[SPHEREx Explanatory Supplement](https://irsa.ipac.caltech.edu/data/SPHEREx/docs/spherex_explanatory_supplement.pdf)
